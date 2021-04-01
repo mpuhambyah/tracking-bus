b31e4244d80fe8e7a1f85133cea96ab1adb7e9b6
mpuhambyah <mpu.hambyah@gmail.com>
2021-04-01 10:25:39 +0700

    config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://riset.its.ac.id/tracking-bus';
+$config['base_url'] = 'https://riset.its.ac.id/electricbus';
 
 /*
 |--------------------------------------------------------------------------
