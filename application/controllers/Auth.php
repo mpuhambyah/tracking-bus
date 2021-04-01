9b03233bd202eabf1c5b973311c06c0d618c8309
mpuhambyah <mpu.hambyah@gmail.com>
2021-04-01 22:33:17 +0700

    sorry bay, auth e mau tak utek utek

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -3,24 +3,10 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-    public function get_client_ip()
-    {
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {   //check ip from share internet
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {   //to check ip is pass from proxy
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
-        return $ip;
-    }
-
     public function __construct()
     {
         parent::__construct();
         $this->load->library('form_validation');
-        var_dump($this->get_client_ip());
-        die;
     }
 
     public function index()
