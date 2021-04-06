3699a94cb1f0ae9ae512561d62cb484c2a73e0cc
bayuuat <bayuuat@gmail.com>
2021-04-06 22:15:54 +0700

    tambah sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,10 +1,68 @@
 <div id="wrapper">
-
-    <aside id="sidebar-wrapper">
-        <div class="sidebar-brand">
-            <a id="sidebar-toggle">BUS X</a>
+    <aside id="sidebar-wrapper" class="shadow-lg p-3 mb-5">
+        <div class="sidebar-brand mb-3">
+            <a id="sidebar-toggle" class="text-danger">CLOSE X</a>
         </div>
-        <div class="sidebar-nav">
+        <img src="https://media.wired.com/photos/5cf832279c2a7cd3975976ca/master/pass/Transpo_XcelsiorChargeCharging_TA.jpg"
+            alt="bus-image" class="img-fluid mb-3" style="border-radius: 15px;">
+        <h4 class="text-center bus-name">ELECTRIC BUS</h4>
+        <div class="container-data px-3">
+            <table class="table table-borderless">
+                <tr>
+                    <td><i class="fas fa-tachometer-alt"></i></td>
+                    <td>
+                        <h6>Speed</h6>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="pt-0"><span>55 Km/h</span></td>
+                </tr>
+                <tr>
+                    <td class="pt-4"><i class="fas fa-map-marker-alt"></i></td>
+                    <td class="pt-4">
+                        <h6>Location</h6>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="pt-0"><span>Bundaran ITS</span></td>
+                </tr>
+                <tr>
+                    <td class="pt-4"><i class="fas fa-clock"></i></td>
+                    <td class="pt-4">
+                        <h6>Departure Time</h6>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="pt-0"><span>12:30 PM</span></td>
+                </tr>
+            </table>
+            <hr>
+            <table class="table table-borderless jadwal">
+                <tr>
+                    <td><i class="fas fa-arrow-up"></i></td>
+                    <td class="pb-0">
+                        <h6 class="mb-0">Departure</h6>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="pt-0"><span>Halte ITS</span></td>
+                </tr>
+                <tr>
+                    <td class="pt-4"><i class="fas fa-arrow-down"></i></td>
+                    <td class="pt-4">
+                        <h6>Arriving</h6>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td class="pt-0"><span>Halte UNAIR</span></td>
+                </tr>
+            </table>
 
         </div>
     </aside>
