5a391d59928920e7e0c896ae44b6caaea0f7ec7f
mpuhambyah <mpu.hambyah@gmail.com>
2021-04-23 13:37:50 +0700

    base_url

--- application/config/config.php
+++ application/config/config.php
@@ -24,11 +24,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 
-// @$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-// @$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
-// @$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-
-$config['base_url'] = 'https://riset.its.ac.id/electricbus/';
+@$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+@$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+@$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
