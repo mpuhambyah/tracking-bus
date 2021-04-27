473dad09df341b312ce986810119025c81827c58
mpuhambyah <mpu.hambyah@gmail.com>
2021-04-27 21:51:51 +0700

    limit history

--- application/models/M_history.php
+++ application/models/M_history.php
@@ -6,7 +6,7 @@ class M_history extends CI_Model
   public function getPolyline($id)
   {
     $query = "SELECT `bus_location`.`id`,`bus_location`.`latitude`,`bus_location`.`longitude` FROM `bus_location` 
-    WHERE `bus_location`.`id_bus` = $id ORDER BY `bus_location`.`time` DESC LIMIT 100";
+    WHERE `bus_location`.`id_bus` = $id ORDER BY `bus_location`.`time` DESC LIMIT 10";
     return $this->db->query($query)->result_array();
   }
 }
