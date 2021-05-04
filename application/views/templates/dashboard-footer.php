945673852ea26c027664b515fe0abd914097bcbc
mpuhambyah <mpu.hambyah@gmail.com>
2021-05-04 17:00:44 +0700

    heading debugging

--- application/views/templates/dashboard-footer.php
+++ application/views/templates/dashboard-footer.php
@@ -25,7 +25,6 @@
 <script src="<?= base_url(); ?>assets/vendor/jquery/jquery.min.js"></script>
 <script src="<?= base_url(); ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 <script src="<?= base_url(); ?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?= base_url(); ?>assets/js/script.js"></script>
 <script src="<?= base_url(); ?>assets/vendor/datatables/jquery.dataTables.min.js"></script>
 <script src="<?= base_url(); ?>assets/vendor/datatables/dataTables.bootstrap4.js"></script>
 
