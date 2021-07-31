a50be3aee3103f1e515bb8c45dd60ced122ac9d8
mpuhambyah <mpu.hambyah@gmail.com>
2021-07-31 17:57:11 +0700

    panel done

--- application/controllers/PanelBus.php
+++ application/controllers/PanelBus.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class PanelBus extends CI_Controller
+class Panelbus extends CI_Controller
 {
     public function __construct()
     {
